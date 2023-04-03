f494996059610833f5a6cd8acb0be19658f0df0d
Navedya Ojha <68197201+Navedya06@users.noreply.github.com>
2023-04-03 11:07:01 +0530

    Update streamlit_app.py
    edit descriptions.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,10 @@ import streamlit as st
 
 st.title("My parents new healthy Diner")
 
-st.header("Breakfast Menu")
-st.write("Omega 3 & blueberry oatmeal")
-st.write("Kale, Spinach and Rocket smoothie")
-st.write("Hard-boiled free-range egg")
+st.header("Breakfast Favorites:")
+st.write("🥣 Omega 3 & blueberry oatmeal")
+st.write("🥗 Kale, Spinach and Rocket smoothie")
+st.write("🐔 Hard-boiled free-range egg")
+st.write("🥑🍞 Avacado Toast")
+
+st.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
