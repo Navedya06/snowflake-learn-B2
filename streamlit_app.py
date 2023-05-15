becaf188b12153aff2dcae935dd3c0fc175799ec
Navedya Ojha <68197201+Navedya06@users.noreply.github.com>
2023-05-16 01:10:19 +0530

    creating function to fetch fruit data

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,19 +37,22 @@ st. dataframe(fruits_to_show)
 
 
 # New Section
+def get_fruityvice_data(this_fruit_choice):
+  # pass user input into API call
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +this_fruit_choice)
+    # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    # st.text(fruityvice_response.json()) #just writes the data to the screen
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json()) #normalize the data
+    return fruityvice_normalized
 st.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = st.text_input('What fruit would you like information about?')
   if not fruit_choice:
     st.error('Please select a fruit to get inofrmation.')
   else:
-    # pass user input into API call
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-    # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    # st.text(fruityvice_response.json()) #just writes the data to the screen
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json()) #normalize the data
+    back_from_function = get_fruityvice_data(fruit_choice)
     # print as table
-    st.dataframe(fruityvice_normalized)
+    st.dataframe(back_from_function)
 except URLError as e:
   st.error()
 
